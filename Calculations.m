0ce657df92dc5fcc0467f30428fc62ab2c04479f
jibinmathews8 <jibinmathews8@gmail.com>
2023-02-13 21:29:42 -0600

    Create Calculations.m

---
+++ Calculations.m
@@ -0,0 +1,42 @@
+%{
+Project Lab 4 Calculations
+%} 
+
+
+HP = 10; % Motor Horsepower
+Motor_V = 480; % Motor VOltage
+Motor_Phase = 3; % 3 Phase Motor
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+%% References
+%{
+1. https://voltage-disturbance.com/power-engineering/voltage-sag-due-to-induction-motor-starting/
+%}
