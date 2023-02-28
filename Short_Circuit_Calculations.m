ece3207e4ef3979dfdb37f1d50c00f74bf6e6049
jibinmathews8 <jibinmathews8@gmail.com>
2023-02-27 18:11:25 -0600

    Update Short_Circuit_Calculations.m

--- Short_Circuit_Calculations.m
+++ Short_Circuit_Calculations.m
@@ -2,3 +2,14 @@
 Project Lab 4 
 Short Circuit Calculations
 %} 
+
+%{
+
+Info Needed:
+    Motor Voltage
+    Motor HP
+    NEMA code letter
+    Source Transformer
+    Source Tansformer % Impedance
+
+%}
